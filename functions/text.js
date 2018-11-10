32ba17d9a0c1d7aac9b7e5ea8063a9c728eeb067
Akin <akinkunmishonibare@gmail.com>
2018-11-10 06:38:53 -0500

    wip translate

--- functions/text.js
+++ functions/text.js
@@ -24,32 +24,32 @@ module.exports = async context => {
 };
 
 function translate(stringToTranslate, languageTo = "french") {
-  translateResult = "dickhead";
+  // Instantiates a client
+  const translate = new Translate({
+    projectId: process.env.GOOGLE_PROJECT_ID
+  });
 
-  return translateResult;
-  // // Instantiates a client
-  // const translate = new Translate({
-  //   projectId: process.env.GOOGLE_PROJECT_ID
-  // });
-  // let rawdata = fs.readFileSync("../languageCode.json");
-  // let languages = JSON.parse(rawdata);
-  // for (var i = 0; i < languages.length; i++) {
-  //   if (languageTo.toLowerCase() === languages[i]["name"].toLowerCase()) {
-  //     var langCode = languages[i]["code"];
-  //   }
-  // }
+  let rawdata = fs.readFileSync("../languageCode.json");
+  let languages = JSON.parse(rawdata);
 
-  // return await new Promise((resolve, reject) => {
-  //   translate
-  //     .translate(stringToTranslate, langCode)
-  //     .then(results => {
-  //       const translation = results[0];
-  //       console.log(`Text: ${stringToTranslate}`);
-  //       console.log(`Translation: ${translation}`);
-  //       resolve(translation);
-  //     })
-  //     .catch(err => {
-  //       console.error("ERROR:", err);
-  //     });
-  // });
+  for (var i = 0; i < languages.length; i++) {
+    if (languageTo.toLowerCase() === languages[i]["name"].toLowerCase()) {
+      var langCode = languages[i]["code"];
+    }
+  }
+
+
+  return await new Promise((resolve, reject) => {
+    translate
+      .translate(stringToTranslate, langCode)
+      .then(results => {
+        const translation = results[0];
+        console.log(`Text: ${stringToTranslate}`);
+        console.log(`Translation: ${translation}`);
+        resolve(translation);
+      })
+      .catch(err => {
+        console.error("ERROR:", err);
+      });
+  });
 }
