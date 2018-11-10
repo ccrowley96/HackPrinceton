c58e4230258accc60792a114689371c0989881d0
Akin <akinkunmishonibare@gmail.com>
2018-11-10 06:24:37 -0500

    missing twilio shit

--- functions/text.js
+++ functions/text.js
@@ -1,52 +1,55 @@
-const lib = require('lib');
+const lib = require("lib");
 const acccountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_TOKEN;
-const client = require('twilio')(acccountSid, authToken);
+const client = require("twilio")(acccountSid, authToken);
+const { Translate } = require("@google-cloud/translate");
+const fs = require("fs");
 
-module.exports = async (context) => {
-   let text = context["params"].Body;
-   let sender = context["params"].From;
-   let result = "Failed...";
+module.exports = async context => {
+  let text = context["params"].Body;
+  let sender = context["params"].From;
+  let result = "Failed...";
 
-
-   if(text.includes("translate")){
-        result = await translate(text)
-   }
-
-   await client.messages.create({
-       to: sender,
-       from: '+14318004137',
-       body: result
-   }).then(msg => console.log(msg.sid))
-};
-
-async function translate(text) {
-    translateResult = "";
-
-  // Instantiates a client
-  const translate = new Translate({
-    projectId: process.env.GOOGLE_PROJECT_ID
-  });
-  let rawdata = fs.readFileSync("../languageCode.json");
-  let languages = JSON.parse(rawdata);
-  for (var i = 0; i < languages.length; i++) {
-    if (languageTo.toLowerCase() === languages[i]["name"].toLowerCase()) {
-      var langCode = languages[i]["code"];
-    }
+  if (text.includes("translate")) {
+    result = translate(text);
   }
 
-  return await new Promise((resolve, reject) => {
-    translate
-      .translate(stringToTranslate, langCode)
-      .then(results => {
-        const translation = results[0];
-        console.log(`Text: ${stringToTranslate}`);
-        console.log(`Translation: ${translation}`);
-        resolve(translation);
-      })
-      .catch(err => {
-        console.error("ERROR:", err);
-      });
-  });
+  await client.messages
+    .create({
+      to: sender,
+      from: "+14318004137",
+      body: result
+    })
+    .then(msg => console.log(msg.sid));
+};
 
+function translate(stringToTranslate, languageTo = "french") {
+  translateResult = "dickhead";
+
+  return translateResult;
+  // // Instantiates a client
+  // const translate = new Translate({
+  //   projectId: process.env.GOOGLE_PROJECT_ID
+  // });
+  // let rawdata = fs.readFileSync("../languageCode.json");
+  // let languages = JSON.parse(rawdata);
+  // for (var i = 0; i < languages.length; i++) {
+  //   if (languageTo.toLowerCase() === languages[i]["name"].toLowerCase()) {
+  //     var langCode = languages[i]["code"];
+  //   }
+  // }
+
+  // return await new Promise((resolve, reject) => {
+  //   translate
+  //     .translate(stringToTranslate, langCode)
+  //     .then(results => {
+  //       const translation = results[0];
+  //       console.log(`Text: ${stringToTranslate}`);
+  //       console.log(`Translation: ${translation}`);
+  //       resolve(translation);
+  //     })
+  //     .catch(err => {
+  //       console.error("ERROR:", err);
+  //     });
+  // });
 }
