e067f1ad4890388d8b43f8e564f02c6e3e16f30d
Cory <ccrowley96@gmail.com>
2018-11-10 17:35:33 -0500

    wip directions

--- functions/messaging/directions.js
+++ functions/messaging/directions.js
@@ -1,17 +1,30 @@
 const axios = require('axios');
+const striptags = require('striptags');
+let directionList = [];
 
+// Takes direction request and returns google directions API list of directions
+// Format: direction [walk | drive] from [source] to [destination]
 module.exports = (tel = "", body = "", from = {}, to = {}, callback) => {
-
     let res = '';
-    axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=${process.env.MAPS_API_KEY}`)
+
+    let origin_addr = encodeURIComponent(body.substring(body.indexOf("from") + 5, body.lastIndexOf(" to ")).trim());
+    let dest_addr = encodeURIComponent(body.substring(body.lastIndexOf(" to ") + 4, body.length).trim());
+
+    axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=${source}&destination=${dest}&key=${process.env.MAPS_API_KEY}`)
     .then(function (response) {
         //Parse Directions
         response.data.routes[0].legs[0].steps.forEach(element => {
-            console.log(element.html_instructions);
-            res += element.html_instructions + element.distance.text + "\n";
-         })
+            let curr_step = element.html_instructions +" ("+ element.distance.text+")";
+            let regex = /(&nbsp;|<([^>]+)>)/ig;
+            directionList.push(striptags(curr_step,["div"]).replace(regex, " "));
+            res = directionList.join('\n\n');
+         });
     }).then(() =>{
-        callback(null, res);
+        if(res.length > 1500){
+            callback(null, "Directions limited to 1500 chars...\n" + res.substr(0,1500) + "\n.....");
+        }else{
+            callback(null, res);
+        }
     })
     .catch(function (error) {
         console.log(error);
