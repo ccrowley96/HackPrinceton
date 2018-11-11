d4f8b7cf03dcef78c02bf23df8d2c119cda731be
Cory <ccrowley96@gmail.com>
2018-11-10 21:40:33 -0500

    fixed formatting and updated help

--- functions/messaging/textnet.js
+++ functions/messaging/textnet.js
@@ -6,9 +6,9 @@ module.exports = (tel = "", body = "", from = {}, to = {}, callback) => {
         '\n1. "textnet" - returns lists of available functions',
         '\n\n2. "direction" - format: "direction [walk | drive | transit | bike] from [source] to [destination]"',
         '\n\n3. "translate" - format: "translate [string to translate] to [target language]"',
-        '\n\n4. "recommend" - format: "Search term, for example "food" or "restaurants". The term may also be business names, such as "Starbucks\
-                \nLocation, for example "New York City", "NYC", "350 5th Ave,"\
-                \nExample: recommend coffee shop near Times Square New York City'
+        '\n\n4. "recommend" - format: "recommend [business] near [location]"\
+                \nBusiness - for example "food" or "restaurants". The term may also be business names, such as "Starbucks\
+                \nLocation, for example "New York City", "NYC", "350 5th Ave,"'
       ].join("\n")
     );
   };
