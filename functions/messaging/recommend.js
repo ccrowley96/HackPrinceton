45d6440fce8f18780d45650f745075bcfe41fd87
Cory <ccrowley96@gmail.com>
2018-11-10 21:30:21 -0500

    working local test on yelp recommendations

--- functions/messaging/recommend.js
+++ functions/messaging/recommend.js
@@ -1,26 +1,39 @@
-const yelp = require('yelp-fusion');
-const apiKey = process.env.YELP_API_KEY;
+const axios = require('axios');
 
 module.exports = (tel = "", body = "", from = {}, to = {}, callback) => {
+  let input_term = encodeURIComponent(body.substring(body.indexOf("recommend") + 10, body.lastIndexOf(" near ")).trim());
+  let input_location = encodeURIComponent(body.substring(body.lastIndexOf(" near ") + 6, body.length).trim());
 
-    // recommend [coffee shop] near [location]
-    let input_term = encodeURIComponent(body.substring(body.indexOf("recommend") + 10, body.lastIndexOf(" near ")).trim());
-    let input_location = encodeURIComponent(body.substring(body.lastIndexOf(" near ") + 6, body.length).trim());
+  let yelp_req = `https://api.yelp.com/v3/businesses/search?term=${input_term}&location=${input_location}&limit=5&open_now=true`;
 
-    const searchRequest = {
-      term: input_term,
-      location: input_location
-    };
-
-    const client = yelp.client(apiKey);
-    
-    client.search(searchRequest).then(response => {
-      const firstResult = response.jsonBody.businesses[0];
-      const prettyJson = JSON.stringify(firstResult, null, 4);
-      console.log(prettyJson);
-      callback(null,prettyJson);
-    }).catch(e => {
-      console.log(e);
-    });
-  };
+  let res_arr = [];
+  let result = '';
+  axios.get(yelp_req,
+  {
+      headers: {"Authorization" : `Bearer ${process.env.YELP_API_KEY}`}
+  })
+  .then((response) => {
+      let count = 1;
+      response.data.businesses.forEach(el => {
+          let biz = {
+              name: el.name,
+              rating: el.rating,
+              distance: `${Math.round(el.distance)} meters`,
+              display_phone: el.display_phone,
+              address: `${el.location.address1}, ${el.location.city} ${el.location.state}`
+          };
+          res_arr.push(biz);
+          result += `${count}: Name: ${biz.name} 
+              \tRating: ${biz.rating} / 5
+              \tDistance: ${biz.distance}
+              \tPhone: ${biz.display_phone}
+              \tAddress: ${biz.address}\n\n`;
+          count++;
+      });
+  })
+  .then(() => callback(null, result))
+  .catch(function (error) {
+      console.log('error :(');
+  });   
+};
   
